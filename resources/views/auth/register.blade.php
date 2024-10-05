93a131a213c87d7351f2600e794a5ee51a52fcb2
joshblevins1985 <55040547+joshblevins1985@users.noreply.github.com>
2024-10-05 16:05:26 -0400

    Initial Commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,7 +28,7 @@
 
                 <div class="signin-form">
                     <h2 class="form-title">Sign up</h2>
-                    <form method="POST" class="register-form" id="login-form">
+                    <form action="<?= url('register') ?>" method="post" id="registration-form" autocomplete="off" class="register-form">
                         <div class="form-group">
                             <label for="email"><i class="zmdi zmdi-account material-icons-name"></i></label>
                             <input type="text" name="email" id="email" value="{{ old('email') }}" placeholder="Your Email"/>
